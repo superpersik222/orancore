92f94e30ad2ae932e262c5c4fb687879bca08e01
Graur Efim <efim.graur@alpiq.com>
2025-02-22 21:26:38 +0100

    add Dockerfile

--- src/components/Blog/ViewCounter.js
+++ src/components/Blog/ViewCounter.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 import { createClient } from '@supabase/supabase-js'
 
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+//const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabase = createClient("https://kprcvuhsklebeuwgstcy.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwcmN2dWhza2xlYmV1d2dzdGN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzNzY4MjgsImV4cCI6MjA1NDk1MjgyOH0.hDH-uzFGwJIhKTOCd-4Qc1bXjDD_YJs7Q-nrvyfMcDk");
 
 const ViewCounter = ({ slug, noCount = false, showCount = true }) => {
   const [views, setViews] = useState(0);
